3839b413d6c16763a7c77f05d8e7cc492a9eccb9
GForceSquared <galaxyforce2000@gmail.com>
2025-03-19 21:23:18 -0400

    Add pybullet-blocks to repo

---
+++ tests/planning_models/test_action.py
@@ -0,0 +1,148 @@
+"""Tests for action.py."""
+
+import pytest
+from task_then_motion_planning.planning import TaskThenMotionPlanner
+
+from pybullet_blocks.envs.block_stacking_env import BlockStackingPyBulletBlocksEnv
+from pybullet_blocks.envs.clear_and_place_env import (
+    ClearAndPlacePyBulletBlocksEnv,
+    ClearAndPlaceSceneDescription,
+)
+from pybullet_blocks.envs.pick_place_env import PickPlacePyBulletBlocksEnv
+from pybullet_blocks.planning_models.action import get_active_operators_and_skills
+from pybullet_blocks.planning_models.perception import (
+    PREDICATES,
+    TYPES,
+    BlockStackingPyBulletBlocksPerceiver,
+    ClearAndPlacePyBulletBlocksPerceiver,
+    PickPlacePyBulletBlocksPerceiver,
+)
+
+
+def test_pick_place_pybullet_blocks_action():
+    """Tests task then motion planning in PickPlacePyBulletBlocksEnv()."""
+
+    env = PickPlacePyBulletBlocksEnv(use_gui=False)
+    sim = PickPlacePyBulletBlocksEnv(env.scene_description, use_gui=True)
+
+    # from gymnasium.wrappers import RecordVideo
+    # env = RecordVideo(env, "videos/pick-place-ttmp-test")
+    max_motion_planning_time = 5  # increase for prettier videos
+
+    perceiver = PickPlacePyBulletBlocksPerceiver(sim)
+    operators, skill_types = get_active_operators_and_skills()
+    skills = {
+        s(sim, max_motion_planning_time=max_motion_planning_time) for s in skill_types
+    }
+
+    # Create the planner.
+    planner = TaskThenMotionPlanner(
+        TYPES, PREDICATES, perceiver, operators, skills, planner_id="pyperplan"
+    )
+
+    # Run an episode.
+    obs, info = env.reset(seed=123)
+    planner.reset(obs, info)
+    for _ in range(10000):  # should terminate earlier
+        action = planner.step(obs)
+        obs, reward, done, _, _ = env.step(action)
+        if done:  # goal reached!
+            assert reward > 0
+            break
+    else:
+        assert False, "Goal not reached"
+
+    env.close()
+
+
+def test_block_stacking_pybullet_blocks_action():
+    """Tests task then motion planning in BlockStackingPyBulletBlocksEnv()."""
+
+    env = BlockStackingPyBulletBlocksEnv(use_gui=False)
+    sim = BlockStackingPyBulletBlocksEnv(env.scene_description, use_gui=True)
+
+    # from gymnasium.wrappers import RecordVideo
+    # env = RecordVideo(env, "videos/block-stacking-ttmp-test")
+    max_motion_planning_time = 0.1  # increase for prettier videos
+
+    perceiver = BlockStackingPyBulletBlocksPerceiver(sim)
+    operators, skill_types = get_active_operators_and_skills()
+    skills = {
+        s(sim, max_motion_planning_time=max_motion_planning_time) for s in skill_types
+    }
+
+    # Create the planner.
+    planner = TaskThenMotionPlanner(
+        TYPES, PREDICATES, perceiver, operators, skills, planner_id="pyperplan"
+    )
+
+    # Run an episode.
+    obs, info = env.reset(
+        seed=1000,
+        #options={"init_piles": [["I"], ["H"], ["M"]]}
+    )
+    planner.reset(obs, info)
+    for _ in range(10000):  # should terminate earlier
+        action = planner.step(obs)
+        obs, reward, done, _, _ = env.step(action)
+        if done:  # goal reached!
+            assert reward > 0
+            break
+    else:
+        assert False, "Goal not reached"
+
+    env.close()
+
+
+@pytest.mark.skip(reason="No plan generated without customized operators.")
+def test_clear_and_place_pybullet_blocks_action():
+    """Tests task then motion planning in ClearAndPlacePyBulletBlocksEnv()."""
+
+    seed = 123
+
+    scene_description = ClearAndPlaceSceneDescription(
+        num_obstacle_blocks=3,
+        stack_blocks=True,
+    )
+
+    env = ClearAndPlacePyBulletBlocksEnv(
+        scene_description=scene_description,
+        use_gui=False,
+        seed=seed,
+    )
+    sim = ClearAndPlacePyBulletBlocksEnv(
+        scene_description=scene_description,
+        use_gui=False,
+        seed=seed,
+    )
+
+    # from gymnasium.wrappers import RecordVideo
+    # env = RecordVideo(env, "videos/clear-and-place-ttmp-test")
+    max_motion_planning_time = 0.1  # increase for prettier videos
+
+    perceiver = ClearAndPlacePyBulletBlocksPerceiver(sim)
+    operators, skill_types = get_active_operators_and_skills(
+        include_improvisational_models=False
+    )
+    skills = {
+        s(sim, max_motion_planning_time=max_motion_planning_time) for s in skill_types
+    }
+
+    # Create the planner
+    planner = TaskThenMotionPlanner(
+        TYPES, PREDICATES, perceiver, operators, skills, planner_id="pyperplan"
+    )
+
+    # Run an episode
+    obs, info = env.reset(seed=seed)
+    planner.reset(obs, info)
+    for _ in range(10000):  # should terminate earlier
+        action = planner.step(obs)
+        obs, reward, done, _, _ = env.step(action)
+        if done:  # goal reached!
+            assert reward > 0
+            break
+    else:
+        assert False, "Goal not reached"
+
+    env.close()
---
+++ tests/planning_models/test_perception.py
@@ -0,0 +1,76 @@
+"""Tests for perception.py."""
+
+from pybullet_blocks.envs.block_stacking_env import BlockStackingPyBulletBlocksEnv
+from pybullet_blocks.envs.clear_and_place_env import (
+    ClearAndPlacePyBulletBlocksEnv,
+    ClearAndPlaceSceneDescription,
+)
+from pybullet_blocks.envs.pick_place_env import PickPlacePyBulletBlocksEnv
+from pybullet_blocks.planning_models.perception import (
+    BlockStackingPyBulletBlocksPerceiver,
+    ClearAndPlacePyBulletBlocksPerceiver,
+    PickPlacePyBulletBlocksPerceiver,
+)
+
+
+def test_pick_place_pybullet_blocks_perceiver():
+    """Tests for PickPlacePyBulletBlocksPerceiver()."""
+
+    env = PickPlacePyBulletBlocksEnv(use_gui=False)
+    sim = PickPlacePyBulletBlocksEnv(env.scene_description, use_gui=False)
+    perceiver = PickPlacePyBulletBlocksPerceiver(sim)
+
+    obs, info = env.reset(seed=123)
+    objects, atoms, goal = perceiver.reset(obs, info)
+    assert len(objects) == 4
+    assert (
+        str(sorted(atoms))
+        == "[(Clear block), (Clear table), (Clear target), (GripperEmpty robot), (IsMovable block), (NotIsMovable table), (NotIsMovable target), (NothingOn block), (NothingOn target), (On block table), (On target table)]"  # pylint: disable=line-too-long
+    )
+    assert str(sorted(goal)) == "[(On block target)]"
+
+
+def test_block_stacking_pybullet_blocks_perceiver():
+    """Tests for BlockStackingPyBulletBlocksPerceiver()."""
+
+    env = BlockStackingPyBulletBlocksEnv(use_gui=False)
+    sim = BlockStackingPyBulletBlocksEnv(env.scene_description, use_gui=True)
+    perceiver = BlockStackingPyBulletBlocksPerceiver(sim)
+
+    obs, info = env.reset(
+        seed=123,
+        options={
+            "init_piles": [["A", "B", "C", "D"]],
+            "goal_piles": [["C", "A"], ["D", "B"]],
+        },
+    )
+    objects, atoms, goal = perceiver.reset(obs, info)
+    assert len(objects) == 6
+    assert (
+        str(sorted(atoms))
+        == "[(Clear D), (Clear table), (GripperEmpty robot), (IsMovable A), (IsMovable B), (IsMovable C), (IsMovable D), (NotIsMovable table), (NothingOn D), (On A table), (On B A), (On C B), (On D C)]"  # pylint: disable=line-too-long
+    )
+    assert str(sorted(goal)) == "[(On A C), (On B D)]"
+
+
+def test_clear_and_place_pybullet_blocks_perceiver():
+    """Tests for ClearAndPlacePyBulletBlocksPerceiver()."""
+    scene_description = ClearAndPlaceSceneDescription(
+        num_obstacle_blocks=3,
+        stack_blocks=True,
+    )
+    env = ClearAndPlacePyBulletBlocksEnv(
+        scene_description=scene_description, use_gui=False
+    )
+    sim = ClearAndPlacePyBulletBlocksEnv(
+        scene_description=scene_description, use_gui=False
+    )
+    perceiver = ClearAndPlacePyBulletBlocksPerceiver(sim)
+    obs, info = env.reset(seed=124)
+    objects, atoms, goal = perceiver.reset(obs, info)
+    assert len(objects) == 7
+    assert (
+        str(sorted(atoms))
+        == "[(Clear C), (Clear T), (Clear table), (GripperEmpty robot), (IsMovable A), (IsMovable B), (IsMovable C), (IsMovable T), (NotIsMovable table), (NotIsMovable target), (NothingOn C), (NothingOn T), (On A target), (On B A), (On C B), (On T table), (On target table)]"  # pylint: disable=line-too-long
+    )
+    assert str(sorted(goal)) == "[(On T target)]"
