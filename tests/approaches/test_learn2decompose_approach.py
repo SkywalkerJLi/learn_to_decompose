0197696aa6eed85d25fe4ea7fb13469fdb5f5d11
GForceSquared <galaxyforce2000@gmail.com>
2025-03-24 19:44:39 -0400

    Added new symbolic env

--- tests/approaches/test_learn2decompose_approach.py
+++ tests/approaches/test_learn2decompose_approach.py
@@ -6,27 +6,27 @@ from task_then_motion_planning.planning import TaskThenMotionPlanner
 
 from python_research_starter.approaches.learn2decompose_approach import Learn2DecomposeApproach
 from python_research_starter.benchmarks.maze_benchmark import MazeBenchmark
-from pybullet_blocks.envs.block_stacking_env import BlockStackingPyBulletBlocksEnv
+from pybullet_blocks.envs.symbolic_block_stacking_env import SymbolicBlockStackingPyBulletBlocksEnv
 from python_research_starter.utils import plan_is_valid
 
 from pybullet_blocks.planning_models.action import get_active_operators_and_skills
 from pybullet_blocks.planning_models.perception import (
     PREDICATES,
     TYPES,
-    BlockStackingPyBulletBlocksPerceiver,
+    SymbolicBlockStackingPyBulletBlocksPerceiver,
 )
 
 def test_learn2decompose_approach():
     """Tests Learn2Decompose planning in BlockStackingPyBulletBlocksEnv()."""
 
-    env = BlockStackingPyBulletBlocksEnv(use_gui=False)
-    sim = BlockStackingPyBulletBlocksEnv(env.scene_description, use_gui=True)
+    env = SymbolicBlockStackingPyBulletBlocksEnv(use_gui=True)
+    sim = SymbolicBlockStackingPyBulletBlocksEnv(env.scene_description, use_gui=False)
 
     # from gymnasium.wrappers import RecordVideo
     # env = RecordVideo(env, "videos/block-stacking-ttmp-test")
     max_motion_planning_time = 0.1  # increase for prettier videos
 
-    perceiver = BlockStackingPyBulletBlocksPerceiver(sim)
+    perceiver = SymbolicBlockStackingPyBulletBlocksPerceiver(sim)
     operators, skill_types = get_active_operators_and_skills()
     skills = {
         s(sim, max_motion_planning_time=max_motion_planning_time) for s in skill_types
@@ -41,9 +41,13 @@ def test_learn2decompose_approach():
     obs, info = env.reset(
         seed=99,
     )
+    print("OBSERVATIONS")
+    print(obs)
     init_state = env.get_state()
     print("State representation")
     print(init_state)
+
+    # if a block's pose is within a certain bound of another, then it is on top
     init_graph = init_state.to_observation()
     print("Graph Representation")
     print(init_graph)
@@ -52,16 +56,16 @@ def test_learn2decompose_approach():
         action = planner.step(obs)
         obs, reward, done, _, _ = env.step(action)
         state = env.get_state()
-        print(state)
+        # print(state)
         graph = state.to_observation()
-        print(graph)
+        # print(graph)
         if done:  # goal reached!
             assert reward > 0
             break
     else:
         assert False, "Goal not reached"
 
-    # env.close()
+    env.close()
 
     # benchmark = MazeBenchmark(5, 8, 5, 8)
     # approach = Learn2DecomposeApproach(
