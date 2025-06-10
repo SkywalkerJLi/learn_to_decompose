391392b9e7519873613914ba937956dbcb5aefa1
SkywalkerJLi <sl5183@princeton.edu>
2025-06-09 20:47:25 -0400

    construct new scene graphs

--- src/python_research_starter/parallel_subproblem_pipeline/test_learn2decompose_subproblem_testing.py
+++ src/python_research_starter/parallel_subproblem_pipeline/test_learn2decompose_subproblem_testing.py
@@ -24,6 +24,10 @@ import torch
 from python_research_starter.subgoal_pipeline.GNN_Models import get_model
 from python_research_starter.subgoal_pipeline.GraphPairDataset import GraphPairDataset
 
+# Parallel Processing
+from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
+
 print(os.getcwd())
 in_channels = 18
 hidden_channels = 64
@@ -93,6 +97,23 @@ def create_edge_dict(num_nodes):
 """
 def is_subset(list1, list2):
     return set(list1).issubset(set(list2))
+
+
+
+def calculate_plans_parallel(input_list, plan_function, max_workers=None):
+    if max_workers is None:
+        max_workers = multiprocessing.cpu_count()
+    
+    # Use ProcessPoolExecutor for CPU-bound tasks
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        # Submit all tasks
+        futures = [executor.submit(plan_function, graph, subgoal) 
+                  for graph, subgoal in input_list]
+        
+        # Collect results in order
+        results = [future.result() for future in futures]
+    
+    return results
 
 
 """
@@ -190,7 +211,6 @@ def test_learn2decompose_approach(return_edge_links=True):
         edge_index_list.sort()
 
         subgoal_index = 0
-        prev_subgoal_graph = init_graph
 
         ### Only increment index if A is on the bottom, otherwise the robot will have to rebuild anyways from scratch
         if nodes[1][10] == -1:
@@ -212,39 +232,42 @@ def test_learn2decompose_approach(return_edge_links=True):
                          ['A', 'B', 'C', 'D', 'E'], 
                          ['A', 'B', 'C', 'D', 'E', 'F']]
         
-        file_path = ground_truth_subgoal_filepath[subgoal_index]
-        with open(file_path, "rb") as f:
-            subgoal_graph = pickle.load(f)
+        # file_path = ground_truth_subgoal_filepath[subgoal_index]
+        # with open(file_path, "rb") as f:
+        #     subgoal_graph = pickle.load(f)
 
-        ### GNN CODE ###
-        scene_data = GraphPairDataset.convert_data(init_graph, subgoal_graph)
+        # ### GNN CODE ###
+        # scene_data = GraphPairDataset.convert_data(init_graph, subgoal_graph)
 
-        # Forward pass
-        with torch.no_grad():
-            out = mp_model(scene_data.x, scene_data.edge_index, scene_data.edge_attr)
-            importance_scores = torch.sigmoid(out)
+        # # Forward pass
+        # with torch.no_grad():
+        #     out = mp_model(scene_data.x, scene_data.edge_index, scene_data.edge_attr)
+        #     importance_scores = torch.sigmoid(out)
 
-        print("Input graph: ", scene_data.x)
-        print("Input edge_links", scene_data.edge_index)
-        print(f"Node importance scores: {importance_scores.numpy()}")
-        print(f"One-hot scores: {importance_scores.numpy() > 0.5}" )
+        # print("Input graph: ", scene_data.x)
+        # print("Input edge_links", scene_data.edge_index)
+        # print(f"Node importance scores: {importance_scores.numpy()}")
+        # print(f"One-hot scores: {importance_scores.numpy() > 0.5}" )
 
-        # Given the importance scores, create a new goal pile with a reduced subset of blocks
-        # The new goal for the planner is now the subgoal, rather than the entire goal
-        new_info = {'goal_piles': [subgoal_piles[subgoal_index]]}
+        # # Given the importance scores, create a new goal pile with a reduced subset of blocks
+        # # The new goal for the planner is now the subgoal, rather than the entire goal
+        # new_info = {'goal_piles': [subgoal_piles[subgoal_index]]}
 
         importance_thresh = 0.9
 
-        # If the blocks are in the goal pile, they are important
-        for block in subgoal_piles[subgoal_index]:
-            importance_scores[ord(block) - ord('A')] = 1
-        print(importance_scores.numpy())
+        # # If the blocks are in the goal pile, they are important
+        # for block in subgoal_piles[subgoal_index]:
+        #     importance_scores[ord(block) - ord('A')] = 1
+        # print(importance_scores.numpy())
 
         # Given the current scene graph and next subgoal, output the next scene graph with completed subgoal
         # Leave all non important objects where they are
         # If the next subgoal is the final goal, return done
         def generate_scene_graph(current_graph, subgoal_index):
             subgoal = subgoal_piles[subgoal_index]
+            goal = subgoal_piles[len(subgoal_piles) - 1]
+            print('subgoal')
+            print(subgoal)
 
             with open(ground_truth_subgoal_filepath[subgoal_index], "rb") as f:
                 next_subgoal_graph = pickle.load(f)
@@ -257,8 +280,8 @@ def test_learn2decompose_approach(return_edge_links=True):
                 importance_scores = torch.sigmoid(out)
 
             # If the blocks are in the goal pile, they are important
-            for block in subgoal_piles[subgoal_index]:
-                importance_scores[ord(block) - ord('A')] = 1
+            # for block in subgoal_piles[subgoal_index]:
+            #     importance_scores[ord(block) - ord('A')] = 1
 
             # TODO 6/7/2025
             # Place the important blocks in their correct location
@@ -267,7 +290,8 @@ def test_learn2decompose_approach(return_edge_links=True):
             # All other blocks should be placed in a free position
             # Use sample_free_block_pose from base_env to do so
             # Sample free pose for first block
-
+            print("importance scores")
+            print(importance_scores)
             # Sample position for the first important block, checking in order
             block_index = 0
             for score in importance_scores:
@@ -276,13 +300,31 @@ def test_learn2decompose_approach(return_edge_links=True):
                     break
                 block_index += 1
             
-            letter = subgoal[block_index]
-            block_id = env.letter_to_block_id[letter]
-            block_pose = env.sample_free_block_pose(block_id)
-            block_position = block_pose.position
+            # Sample new position for first block
+            if block_index == 0:
+            
+                letter = subgoal[block_index]
+                print('first block letter')
+                print(letter)
+                block_id = env.letter_to_block_id[letter]
+                block_pose = env.sample_free_block_pose(block_id)
+                block_position = block_pose.position
+            else: # Otherwise, get position of current highest block
+                block_index = block_index - 1
+                letter = subgoal[block_index]
+                print('first block letter')
+                print(letter)
+                block_id = env.letter_to_block_id[letter]
+                world_to_block = get_pose(block_id, env.physics_client_id)
+                block_position = world_to_block.position
+
             block_height = 2 * env.scene_description.block_half_extents[2]
             # Stack blocks in subgoal
+            print('stack blocks')
             for i, letter in enumerate(subgoal[block_index + 1:]):
+                # Mark the stacked blocks as unimportant after stacking
+                importance_scores[block_index + i + 1] = 0
+                print(letter)
                 dz = (i + 1) * block_height
                 position = np.add(block_position, (0, 0, dz))
                 block_id = env.letter_to_block_id[letter]
@@ -290,8 +332,10 @@ def test_learn2decompose_approach(return_edge_links=True):
 
             # For all remaining important blocks, sample a free block pose
             for block_index, score in enumerate(importance_scores):
-                if score > importance_scores:
-                    letter = subgoal[block_index]
+                if score > importance_thresh:
+                    letter = goal[block_index]
+                    print('other important blocks')
+                    print(letter)
                     block_id = env.letter_to_block_id[letter]
                     block_pose = env.sample_free_block_pose(block_id)
             
@@ -306,42 +350,40 @@ def test_learn2decompose_approach(return_edge_links=True):
         # Use lambda generators?
         # TODO 6/7/2025
 
-        scene_pairs = []
-        """
-                
-        from concurrent.futures import ProcessPoolExecutor
+        # Generate next scene graphs up to the second to last state
+        current_graph = init_graph
 
-        # Your CPU-heavy motion planning function
-        def generate_motion_plan(scene_graph, goal_state):
-            # Compute and return motion plan
-            ...
+        graph_pairs = []
+        graph_pairs.append((current_graph, {'goal_piles': [subgoal_piles[subgoal_index]]}))
+        while subgoal_index < len(subgoal_piles) - 1:
+            next_graph = generate_scene_graph(current_graph, subgoal_index)
+            subgoal_index += 1
 
-        # List of (scene_graph, goal_state) input pairs
-        
-        input_pairs = [(scene_graph1, goal_state1), (scene_graph2, goal_state2), ...]
+            print('curr graph')
+            print(current_graph)
+            print('next graph')
+            print(next_graph)
 
-        # Wrapper function to unpack each input tuple
-        def wrapper(args):
-            return generate_motion_plan(*args)
+            graph_pairs.append((next_graph, {'goal_piles': [subgoal_piles[subgoal_index]]}))
+            current_graph = next_graph
 
-        with ProcessPoolExecutor() as executor:
-            results = list(executor.map(wrapper, input_pairs))
 
-        """
+        # plans = calculate_plans_parallel(graph_pairs, planner_baseline.reset())
 
-        task_plan = planner.reset(obs, new_info, importance_scores.numpy(), importance_thresh)
-        task_plan_baseline = planner_baseline.reset(obs, new_info)
+        # print(plans)
+        # task_plan = planner.reset(obs, new_info, importance_scores.numpy(), importance_thresh)
+        # task_plan_baseline = planner_baseline.reset(obs, new_info)
 
-        print("Running demo: ", demo)
-        for _ in range(10000):  # should terminate earlier
-            action = planner.step(obs)
-            #action = planner_baseline.step(obs)
-            obs, reward, done, _, _ = env.step(action)
+        # print("Running demo: ", demo)
+        # for _ in range(10000):  # should terminate earlier
+        #     action = planner.step(obs)
+        #     #action = planner_baseline.step(obs)
+        #     obs, reward, done, _, _ = env.step(action)
 
-            if done:  # goal reached!
-                assert reward > 0
-                break
-        else:
-            assert False, "Goal not reached"
+        #     if done:  # goal reached!
+        #         assert reward > 0
+        #         break
+        # else:
+        #     assert False, "Goal not reached"
 
     env.close()
